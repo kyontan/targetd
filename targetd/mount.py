7667c58f08f8068145d39f93f5395cef3de061ef
kyontan <kyontan@monora.me>
2021-06-22 05:08:25 +0900

    Fix typo

--- targetd/mount.py
+++ targetd/mount.py
@@ -11,12 +11,11 @@ class Mount(object):
         Get all currently mounted filesystems typed type
         :return: generator of mount info array of tuple
         """
-        def mounted_filesystems():
-            proc = subprocess.Popen(['mount','-t', 'zfs'],stdout=subprocess.PIPE)
-            while True:
-                line = proc.stdout.readline()
-                if not line:
-                    break
-                # line is like "alice on /alice (zfs, local, nfsv4acls)""
-                words = line.decode().split(" ")
-                yield (words[2], words[0]) # (mountpoint, zfs path)
+        proc = subprocess.Popen(['mount','-t', type],stdout=subprocess.PIPE)
+        while True:
+            line = proc.stdout.readline()
+            if not line:
+                break
+            # line is like "alice on /alice (zfs, local, nfsv4acls)""
+            words = line.decode().split(" ")
+            yield (words[2], words[0]) # (mountpoint, zfs path)
